33e0b5071ee516aa4373306c98945e6d9446d16e
SourTsum <83648290+SourTsum@users.noreply.github.com>
2021-11-18 21:25:45 -0500

    Attempted to get server.

--- sbPlus/index.js
+++ sbPlus/index.js
@@ -1,10 +1,29 @@
 var features = {"mining_speed_sound":true}
 var features_settings = {"msb_time":"&cnot available"}
 var Minesdisplay = new Display();
+//Server Registration
+var serverRec = 0
+register("chat", function(event) {
+    Scoreboard.getLines().forEach(name => {
+    var formattedStr = ChatLib.removeFormatting(name).replace(/[^a-zA-Z -]/g, "")
+    if(formattedStr.includes("The Lift") || formattedStr.includes("The Forge")|| formattedStr.includes("Dwarven Mines")) {
+      serverRec += 1;
+    } else {
+      return
+    }
+  if(serverRec == 0){
+    Minesdisplay.setRenderLoc(1000,1000);
+  }
+  if(serverRec == 1){
+    Minesdisplay.setRenderLoc(10,10);
+    serverRec = 0;
+  }
+  })
+}).setCriteria("&r&7Warping...&r").setParameter("contains");
 //Dwarvin Display
 Minesdisplay.addLine(0,"&r&a&r&6Mining Speed Boost&7: " + features_settings['msb_time']);
 Minesdisplay.setBackground(DisplayHandler.Background.FULL);
-Minesdisplay.setRenderLoc(10, 10);
+Minesdisplay.setRenderLoc(1000, 1000);
 //Dwarvin Features
 register("chat", function(event) {
   World.playSound("mob.silverfish.say", 100, 1);
@@ -17,5 +36,5 @@ register("chat", function(event) {
 }).setCriteria("&r&aYou used your &r&6Mining Speed Boost &r&aPickaxe Ability!&r").setParameter("contains");
 
 register("command",function(event){
-  ChatLib.chat(scrBoard)
+  ChatLib.chat("Yo")
 }).setName("sbp")
